892cd94535dfec7321ed9931d08addf2c6f1c6fb
tim99919 <tim99919@gmail.com>
2019-08-18 12:59:05 +0300

    refactoring

--- src/components/board.js
+++ src/components/board.js
@@ -1,14 +1,4 @@
 export const createBoardTemplate = () => {
-  return `
-    <section class="films">
+  return `<section class="films">
     </section>`;
-};
-
-export const createFilmsListTemplate = (title, content, isExtra, isHidden) => {
-  return `<section class="films-list${isExtra ? `--extra` : ``}">
-    <h2 class="films-list__title ${isHidden ? `visually-hidden` : ``}">${title}</h2>
-    <div class="films-list__container">
-      ${content}
-    </div>
-  </section>`;
 };
