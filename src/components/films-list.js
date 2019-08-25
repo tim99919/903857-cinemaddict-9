483efb8eae7b288549244509883ceb982c04e1b0
tim99919 <tim99919@gmail.com>
2019-08-25 18:02:54 +0300

    film board refactoring

--- src/components/films-list.js
+++
@@ -1,8 +0,0 @@
-export const createFilmsListTemplate = ({title, content, isExtra, isHidden}) => {
-  return `<section class="films-list${isExtra ? `--extra` : ``}">
-    <h2 class="films-list__title ${isHidden ? `visually-hidden` : ``}">${title}</h2>
-    <div class="films-list__container">
-      ${content}
-    </div>
-  </section>`;
-};
--- src/main.js
+++ src/main.js
@@ -16,55 +16,76 @@ const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
 const footerElement = document.querySelector(`.footer`);
 
-const filmsCount = 5;
-const topRatedFilmsCount = 2;
-const mostCommentedFilmsCount = 2;
+const FILMS_COUNT = 15;
+const TOP_RATED_FILMS_COUNT = 2;
+const MOST_COMMENT_FILMS_COUNT = 2;
+const START_FILMS_COUNT = 5;
 
 const renderComponent = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const films = new Array(filmsCount).fill(``).map(() => getFilmCard());
-const topRatedFilms = films.sort((a, b) => b.raiting - a.raiting).slice(0, topRatedFilmsCount);
-const mostCommentedFilms = films.sort((a, b) => b.commentsCount - a.commentsCount).slice(0, mostCommentedFilmsCount);
-// TODO
-// comments section to the bottom of popup
+// const renderFilmsList = (container, count) => {
+//   container.insertAdjacentHTML(
+//     `beforeend`,
+//     films.
+//       splice(0, START_FILMS_COUNT).
+
+//   );
+// };
+
+const films = new Array(FILMS_COUNT).fill(``).map(() => getFilmCard());
+const topRatedFilms = films.sort((a, b) => b.raiting - a.raiting).slice(0, TOP_RATED_FILMS_COUNT);
+const mostCommentedFilms = films.sort((a, b) => b.comments.length - a.comments.length).slice(0, MOST_COMMENT_FILMS_COUNT);
 
 renderComponent(headerElement, createSearchTemplate(), `beforeend`);
 renderComponent(headerElement, createProfileTemplate(getRandomInt(0, 30)), `beforeend`);
 renderComponent(mainElement, createSiteMenuTemplate(films.length), `beforeend`);
 renderComponent(mainElement, createSortingTemplate(), `beforeend`);
-renderComponent(mainElement, createBoardTemplate(), `beforeend`);
+renderComponent(
+    mainElement,
+    createBoardTemplate(
+        films.splice(0, START_FILMS_COUNT).
+          map((it) => createFilmTemplate(it)).
+          join(``),
+        topRatedFilms.map((it) => createFilmTemplate(it)).join(``),
+        mostCommentedFilms.map((it) => createFilmTemplate(it)).join(``)
+    ),
+    `beforeend`
+);
 
-const boardElement = mainElement.querySelector(`.films`);
+// const boardElement = mainElement.querySelector(`.films`);
 
-renderComponent(
-    boardElement,
-    createFilmsListTemplate({
-      title: `All movies. Upcoming`,
-      content: films.map((it) => createFilmTemplate(it)).join(``),
-      isHidden: true
-    }),
-    `beforeend`
-);
-renderComponent(
-    boardElement,
-    createFilmsListTemplate({
-      title: `Top rated`,
-      content: topRatedFilms.map((it) => createFilmTemplate(it)).join(``),
-      isExtra: true
-    }),
-    `beforeend`
-);
-renderComponent(
-    boardElement,
-    createFilmsListTemplate({
-      title: `Most commented`,
-      content: mostCommentedFilms.map((it) => createFilmTemplate(it)).join(``),
-      isExtra: true
-    }),
-    `beforeend`
-);
+// renderComponent(
+//     boardElement,
+//     createFilmsListTemplate({
+//       title: ``,
+//       content: films.
+//         splice(0, START_FILMS_COUNT).
+//         map((it) => createFilmTemplate(it)).
+//         join(``),
+//       isHidden: true
+//     }),
+//     `beforeend`
+// );
+// renderComponent(
+//     boardElement,
+//     createFilmsListTemplate({
+//       title: `Top rated`,
+//       content: topRatedFilms.map((it) => createFilmTemplate(it)).join(``),
+//       isExtra: true
+//     }),
+//     `beforeend`
+// );
+// renderComponent(
+//     boardElement,
+//     createFilmsListTemplate({
+//       title: `Most commented`,
+//       content: mostCommentedFilms.map((it) => createFilmTemplate(it)).join(``),
+//       isExtra: true
+//     }),
+//     `beforeend`
+// );
 
 const filmsListElement = mainElement.querySelector(`.films-list`);
 
